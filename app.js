2432e4e91de97054131e4e1ba85ae54df778bec0
pbt98 <tallbin98@gmail.com>
2019-11-21 14:19:09 +0900

    conn aws database

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'ericagallery.cpi3i6d3shqc.us-east-2.rds.amazonaws.com',
+  user     : 'admin',
+  password : 'databasepbl2!',
+  port     : 3306,
+  database : 'ericagallery'
+});
+
+connection.connect();
+
+connection.query('SELECT * from movie_info', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.', err);
+});
+
+connection.end();
+
 app.prepare()
 .then(() => {
     const server = express();
