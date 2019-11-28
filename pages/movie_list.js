b48b5e251bd256a1a16bc5ec6c7f5d3c4acd1a90
Pjh <jiho5993@naver.com>
2019-11-28 16:02:10 +0900

    Add movie API

--- pages/movie_list.js
+++ pages/movie_list.js
@@ -3,21 +3,23 @@ import Footer from './footer'
 import Movie_list_CSS from '../components/movie_list_css'
 import {useState} from 'react'
 
-const Movie_list = () => {
+import data from '../data/movie_info'
+
+const Movie_list = (props) => {
 
 	const [movie_, setMovie] = useState(true)
 
 	const reserve = (
 		<div class="sort_movie_info">
-			<div class="info_movie">
-				<h2>1</h2>
-				<img src="/movie/blackmoney.jpg" alt="blackmoney" />
-				<div class="information">
-					<div class="movie_name">
-						<p>(12세) 블랙머니</p>
-						<p>예매율 : 10.5% | 평점 : 4.8 | 개봉일 : 2019.09.01</p>
+			{props.data.map(movies => (
+				<div className={"info_movie"}>
+					<h2>1</h2>
+					<img src={movies.movie_info.picture}/>
+					<div class={"information"}>
+						<p>({movies.movie_info.ageRestriction}) {movies.movie_info.movieNm}</p>
+						<p>예매율 : ??% | 평점 : ?? | 개봉일 : {movies.movie_info.openDt}</p>
 					</div>
-					<div class="reserve_button">
+					<div class={"reserve_button"}>
 						<div>
 							<p>줄거리</p>
 							<p>줄거리</p>
@@ -26,133 +28,7 @@ const Movie_list = () => {
 						<button>예매하기</button>
 					</div>
 				</div>
-			</div>
-			<div class="info_movie">
-				<h2>2</h2>
-				<img src="/movie/blackmoney.jpg" alt="blackmoney" />
-				<div class="information">
-					<div class="movie_name">
-						<p>(12세) 블랙머니</p>
-						<p>예매율 : 10.1% | 평점 : 4.8 | 개봉일 : 2019.09.01</p>
-					</div>
-					<div class="reserve_button">
-						<div>
-							<p>줄거리</p>
-							<p>줄거리</p>
-							<p>줄거리</p>
-						</div>
-						<button>예매하기</button>
-					</div>
-				</div>
-			</div>
-			<div class="info_movie">
-				<h2>3</h2>
-				<img src="/movie/blackmoney.jpg" alt="blackmoney" />
-				<div class="information">
-					<div class="movie_name">
-						<p>(12세) 블랙머니</p>
-						<p>예매율 : 10% | 평점 : 4.8 | 개봉일 : 2019.09.01</p>
-					</div>
-					<div class="reserve_button">
-						<div>
-							<p>줄거리</p>
-							<p>줄거리</p>
-							<p>줄거리</p>
-						</div>
-						<button>예매하기</button>
-					</div>
-				</div>
-			</div>
-			<div class="info_movie">
-				<h2>4</h2>
-				<img src="/movie/blackmoney.jpg" alt="blackmoney" />
-				<div class="information">
-					<div class="movie_name">
-						<p>(12세) 블랙머니</p>
-						<p>예매율 : 9.3% | 평점 : 4.8 | 개봉일 : 2019.09.01</p>
-					</div>
-					<div class="reserve_button">
-						<div>
-							<p>줄거리</p>
-							<p>줄거리</p>
-							<p>줄거리</p>
-						</div>
-						<button>예매하기</button>
-					</div>
-				</div>
-			</div>
-			<div class="info_movie">
-				<h2>5</h2>
-				<img src="/movie/blackmoney.jpg" alt="blackmoney" />
-				<div class="information">
-					<div class="movie_name">
-						<p>(12세) 블랙머니</p>
-						<p>예매율 : 9% | 평점 : 4.8 | 개봉일 : 2019.09.01</p>
-					</div>
-					<div class="reserve_button">
-						<div>
-							<p>줄거리</p>
-							<p>줄거리</p>
-							<p>줄거리</p>
-						</div>
-						<button>예매하기</button>
-					</div>
-				</div>
-			</div>
-			<div class="info_movie">
-				<h2>6</h2>
-				<img src="/movie/blackmoney.jpg" alt="blackmoney" />
-				<div class="information">
-					<div class="movie_name">
-						<p>(12세) 블랙머니</p>
-						<p>예매율 : 8.3% | 평점 : 4.9 | 개봉일 : 2019.09.01</p>
-					</div>
-					<div class="reserve_button">
-						<div>
-							<p>줄거리</p>
-							<p>줄거리</p>
-							<p>줄거리</p>
-						</div>
-						<button>예매하기</button>
-					</div>
-				</div>
-			</div>
-			<div class="info_movie">
-				<h2>7</h2>
-				<img src="/movie/blackmoney.jpg" alt="blackmoney" />
-				<div class="information">
-					<div class="movie_name">
-						<p>(12세) 블랙머니</p>
-						<p>예매율 : 7.5% | 평점 : 5 | 개봉일 : 2019.09.01</p>
-					</div>
-					<div class="reserve_button">
-						<div>
-							<p>줄거리</p>
-							<p>줄거리</p>
-							<p>줄거리</p>
-						</div>
-						<button>예매하기</button>
-					</div>
-				</div>
-			</div>
-			<div class="info_movie">
-				<h2>8</h2>
-				<img src="/movie/blackmoney.jpg" alt="blackmoney" />
-				<div class="information">
-					<div class="movie_name">
-						<p>(12세) 블랙머니</p>
-						<p>예매율 : 7% | 평점 : 4.9 | 개봉일 : 2019.09.01</p>
-					</div>
-					<div class="reserve_button">
-						<div>
-							<p>줄거리</p>
-							<p>줄거리</p>
-							<p>줄거리</p>
-						</div>
-						<button>예매하기</button>
-					</div>
-				</div>
-			</div>
+			))}
 		</div>
 	)
 	
@@ -321,6 +197,12 @@ const Movie_list = () => {
       <Footer />
     </div>
   )
+}
+
+Movie_list.getInitialProps = async () => {
+	return {
+		data: data
+	}
 }
 
 export default Movie_list
