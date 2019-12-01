01f306b29322d3b4032c7d8bb29598113d7a9c08
siroch <habizero@gmail.com>
2019-12-01 13:31:53 +0900

    staff_info_details page complete

--- components/staff_info_details_css.js
+++ components/staff_info_details_css.js
@@ -1,6 +1,73 @@
 const Staff_info_details_CSS = () => {
   return (
     <style global jsx>{`
+    	.staff_info_details_header {
+    		margin-top: 5%;
+    		margin-left: 20%;
+    		margin-right: 20%;
+    		display: flex;
+    		justify-content: space-around;
+    		text-align: center;
+    	}
+    	.staff_info_details_header button {
+    		font-size: 12pt;
+    		background-color: black;
+    		color: white;
+    		height: 50%;
+    	}
+    	.staff_info_details_header button:hover {
+    		cursor: pointer;
+    	}
+    	.staff_info_details {
+    		margin-left: 20%;
+    		margin-right: 20%;
+    		border-top: 2px solid black;
+    		border-bottom: 2px solid black;
+    		display: flex;
+    		flex-direction: column;
+    		justify-content: space-around;
+    	}
+    	.staff_info_name {
+    		margin-left: 5%;
+    		margin-right: 5%;
+    		border-bottom: 1px solid black;
+    		padding: 5px;
+    		text-align: center;
+    	}
+    	.details {
+    		display: flex;
+    		flex-direction: row;
+    	}
+    	.work_info {
+    		display: flex;
+    		justify-content: center;
+    		overflow: scroll;
+    		height: 350px;
+    	}
+    	table.type05 {
+        border-collapse: separate;
+        border-spacing: 1px;
+        text-align: left;
+        line-height: 1.5;
+        border-top: 1px solid #ccc;
+        margin: 20px 60px;
+      }
+      table.type05 th {
+        width: 150px;
+        padding: 10px;
+        font-weight: bold;
+        vertical-align: top;
+        border-bottom: 1px solid #ccc;
+        background: black;
+        color: white;
+      }
+      table.type05 td {
+        width: 350px;
+        padding: 10px;
+        vertical-align: top;
+        background: #efefef;
+        border-bottom: 1px solid #ccc;
+      }      
     `}</style>
   )
 }
--- pages/manager/staff_info_details.js
+++ pages/manager/staff_info_details.js
@@ -1,9 +1,150 @@
 import Staff_info_details_CSS from '../../components/staff_info_details_css'
 import Link from 'next/link'
+import {useState} from 'react'
 
 const Staff_info_details = () => {
+	const [score, setScore] = useState(100)
+
 	return (
-		<Staff_info_details_CSS />
+		<div>
+			<Staff_info_details_CSS />
+			<div class="staff_info_details_header">
+				<button>수정완료</button>
+				<h2>직원 상세 정보</h2>
+				<button>뒤로가기</button>
+			</div>
+			<div class="staff_info_details">
+				<div class="staff_info_name">
+					<div class="details">
+						<img src="/reload_arrow.png" alt="xx" />
+						<div>
+							<table class="type05">
+								<tr>
+				    			<th scope="row">이름 | 나이 | 성별</th>
+				    			<td>강동호 | 21세 | 남자</td>
+								</tr>
+								<tr>
+				    			<th scope="row">직무</th>
+				    			<td><input type = "text" value = "기존 직무" name = "work" required  /></td>
+								</tr>
+								<tr>
+				    			<th scope="row">수당</th>
+				    			<td><input type = "text" value = "기존 수당" name = "pay" required  /></td>
+								</tr>
+								<tr>
+				    			<th scope="row">근무 요일</th>
+				    			<td>월,수(12:00~18:00) 금,토(10:00~18:00)</td>
+								</tr>
+								<tr>
+				    			<th scope="row">직원 점수</th>
+				    			<td>{score}점
+				    				<button onClick={() => setScore(score+1)}>+</button>
+				    				<button onClick={() => setScore(score-1)}>-</button>
+				    			</td>
+								</tr>
+							</table>							
+						</div>
+					</div>
+					<h3>근태 정보</h3>
+				</div>
+				<div class="work_info">
+					<table class="type05">
+						<tr>
+		    			<th scope="row">2018.01</th>
+		    			<td>전체 18일 | 출근 17일 | 지각 3일 | 결근 1일</td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.02</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.03</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.04</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.05</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.06</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.07</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.08</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.09</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.10</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.11</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2018.12</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.01</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.02</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.03</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.04</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.05</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.06</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.07</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.08</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.09</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.10</th>
+		    			<td></td>
+						</tr>
+						<tr>
+		    			<th scope="row">2019.11</th>
+		    			<td></td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
 	)
 }
 
