4e6af3dc3b19006202ceb34a19e42c6df9d94b75
duncan2121 <gkstj0604@gamil.com>
2019-12-05 00:37:38 +0900

    Upload rank info

--- components/midcontent_css.js
+++ components/midcontent_css.js
@@ -23,6 +23,74 @@ const Midcontent_CSS = () => {
         display: inline-block;
         margin: 10px;
       }
+      .rank_info{
+        margin-bottom: 5%;
+        border: 3px solid;
+        border-radius: 5px;
+        padding: 10px;
+        display: flex;
+        flex-flow: column;
+        align-items: flex-start;
+      }
+      .ranks{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: baseline;
+      }
+      .rank_b, .rank_s, .rank_g, .rank_p{
+        display: flex;
+        align-items: center;
+      }
+      .ranks h4, .ranks h3, .ranks h2, .ranks h1{
+        margin: 0;
+        margin-bottom: 20px;
+      }
+      .ranks ul{
+        text-align: left;
+        margin: 5px;
+        padding: 0;
+      }
+      .ranks li{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+      }
+      .rank_b li{
+        font-size: 10pt;
+      }
+      .rank_s li{
+        font-size: 11pt;
+      }
+      .rank_g li{
+        font-size: 12pt;
+      }
+      .rank_p li{
+        font-size: 13pt;
+      }
+      .rank_info p{
+        margin: 0;
+        margin-top: 20px;
+        align-self: flex-end;
+        font-size: 10pt;
+        color: gray;
+      }
+      .rank_b img{
+        width: 100px;
+        height: 100px;
+      }
+      .rank_s img{
+        width: 120px;
+        height: 120px;
+      }
+      .rank_g img{
+        width: 150px;
+        height: 150px;
+      }
+      .rank_p img{
+        width: 200px;
+        height: 200px;
+      }
     `}</style>
   )
 }
--- pages/midcontent.js
+++ pages/midcontent.js
@@ -16,6 +16,43 @@ const Midcontent = () => {
         <div className="rank">
           <fieldset>
             <legend><h2>등급 안내</h2></legend>
+            <div className="rank_info">
+              <div className="ranks">
+                <div className="rank_b">
+                  <img src="rank/bronze.png" alt="bronze" />
+                  <ul>
+                    <h4>BRONZE</h4>
+                    <li>1년간 <strong>10만원</strong> 이상</li>
+                    <li>혜택: <strong>3%</strong> 할인</li>
+                  </ul>
+                </div>
+                <div className="rank_s">
+                  <img src="rank/silver.png" alt="silver" />
+                  <ul>
+                    <h3>SILVER</h3>
+                    <li>1년간 <strong>20만원</strong> 이상</li>
+                    <li>혜택: <strong>6%</strong> 할인</li>
+                  </ul>
+                </div>
+                <div className="rank_g">
+                  <img src="rank/gold.png" alt="gold" />
+                  <ul>
+                    <h2>GOLD</h2>
+                    <li>1년간 <strong>40만원</strong> 이상</li>
+                    <li>혜택: <strong>10%</strong> 할인</li>
+                  </ul>
+                </div>
+                <div className="rank_p">
+                  <img src="rank/platinum.png" alt="platinum" />
+                  <ul>
+                    <h1>PLATINUM</h1>
+                    <li>1년간 <strong>70만원</strong> 이상</li>
+                    <li>혜택: <strong>15%</strong> 할인</li>
+                  </ul>
+                </div>
+              </div>
+              <p>* 누적 사용 금액은 1년 단위이며, 할인 혜택은 결제시에만 적용됩니다.(누적 금액은 그대로 계산됩니다.)</p>
+            </div>
           </fieldset>
         </div>
       </div>
